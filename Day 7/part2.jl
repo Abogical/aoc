c2027bdda4894f4f209b50dc563e1a61918dd761
Abdelrahman Abdelrahman <abogical@gmail.com>
2020-12-07 17:51:09 +0200

    Day 7: Parse count as integer immediately

--- Day 7/part2.jl
+++ Day 7/part2.jl
@@ -1,12 +1,9 @@
-rules = Dict{String, Union{Int64, Array{Tuple{SubString{String},SubString{String}}}}}(rule[1] => rule[2] == "no other bags" ? Tuple{SubString{String},SubString{String}}[] : [(color[1], color[2]) for color in eachmatch(r"(\d+) (\S+ \S+) bag", rule[2])] for rule in match.(r"^(\S+ \S+) bags contain ([^\.]+)\.$", eachline("input.txt")))
+rules = Dict{String, Union{Int64, Array{Tuple{Int64,SubString{String}}}}}(rule[1] => rule[2] == "no other bags" ? Tuple{Int64,SubString{String}}[] : [(parse(Int64, color[1]), color[2]) for color in eachmatch(r"(\d+) (\S+ \S+) bag", rule[2])] for rule in match.(r"^(\S+ \S+) bags contain ([^\.]+)\.$", eachline("input.txt")))
 
 function count_bags(color)::Int64
   result = get(rules, color, 0)
   if isa(result, Array)
-    result = isempty(result) ? 0 : sum(begin
-      color_count = parse(Int64, color_count_str)
-      color_count*(count_bags(subcolor) + 1)
-    end for (color_count_str, subcolor) in result)
+    result = isempty(result) ? 0 : sum(color_count*(count_bags(subcolor) + 1) for (color_count, subcolor) in result)
     global rules[color] = result
     return result
   end
