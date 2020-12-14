24dd086b184ecd68c909e3876f3d99254e521382
Abdelrahman Abdelrahman <git@abogic.al>
2020-12-14 03:59:43 +0200

    Day 13: use optimized part 2 solution

--- Day 13/part2.jl
+++ Day 13/part2.jl
@@ -1,15 +1,19 @@
 using Base.Iterators
-using Distributed
 
 lines = readlines("input.txt")
 ids = [id_str == "x" ? BigInt(1) : parse(BigInt, id_str) for id_str in split(lines[2], ',')]
-(max_id, max_id_offset) = findmax(ids)
-time_offsets = collect(Iterators.filter(n -> n != 0, take(countfrom(1-max_id_offset), length(ids))))
-deleteat!(ids, max_id_offset)
-for time in countfrom(BigInt(100000000000000) + BigInt(100000000000000)%max_id, max_id)
-  if all(((time.+time_offsets) .% ids) .== 0)
-    println(time-max_id_offset+1)
-    exit(0)
+
+(time, rest_ids) = Iterators.peel(ids)
+step = time
+
+for id in rest_ids
+  for K in countfrom(time, step)
+    if (K+1) % id == 0
+      global step = lcm(step, id)
+      global time = K+1
+      break
+    end
   end
-  @show time
 end
+
+println(time-length(ids)+1)
